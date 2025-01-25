ca24ad91c5f6f31b1f6ad5698debaaf3f0a249e2
doyooni <ldy033131@gmail.com>
2025-01-25 20:56:54 +0900

    전체 목차 1. 예제 만들기 2. 쓰레드 로컬 - ThreadLocal 3. 템플릿 메서드 패턴과 콜백 패턴 4. 프록시 패턴과 데코레이터 패턴 5. 동적 프록시 기술 6. 스프링이 지원하는 프록시 7. 빈 후처리기 8. @Aspect AOP 9. 스프링 AOP 개념 10. 스프링 AOP 구현 11. 스프링 AOP - 포인트컷 12. 스프링 AOP - 실전 예제 13. 스프링 AOP - 실무 주의사항

---
+++ src/test/java/helo/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,106 @@
+package helo.advanced.trace.strategy;
+
+import helo.advanced.trace.strategy.code.strategy.ContextV1;
+import helo.advanced.trace.strategy.code.strategy.Strategy;
+import helo.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import helo.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+@Slf4j
+public class ContextV1Test {
+    @Test
+    void strategyV0() {
+        logic1();
+        logic2();
+    }
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1); //Context에 원하는걸 주입하고 실행
+        context1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);  //Context에 원하는걸 주입하고 실행
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스1
+     */
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행 익명내부클래스");
+            }
+        };
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 익명내부클래스");
+            }
+        };
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스2
+     */
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴, 람다 (interface에 method 한 개만 있으면 람다사용 가능)
+     */
+    @Test
+    void strategyV4() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
+}
